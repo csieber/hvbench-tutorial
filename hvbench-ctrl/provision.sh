ccaa86150b48c77b6b2dde9749d763e32f64e1f0
Christian Sieber <c.sieber@tum.de>
2016-05-19 08:50:06 -0400

    Added docker to hvbench and hvbench-ctrl box.

--- hvbench-ctrl/provision.sh
+++ hvbench-ctrl/provision.sh
@@ -2,7 +2,14 @@
 
 trap 'exit' ERR
 
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+
 apt-get update
 apt-get update
+
+# Install docker
+apt-get install -y linux-image-extra-$(uname -r)
+apt-get install -y docker-engine
 
 echo "Provision complete (root)"
