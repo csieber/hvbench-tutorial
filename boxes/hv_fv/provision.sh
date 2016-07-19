e74658d83bd67c587b11c7a073fb9fdc4793d84f
mumeranwaar <mumeranwaar@gmail.com>
2016-07-19 14:54:59 +0200

    Missing -y from install python3-setuptools

--- boxes/hv_fv/provision.sh
+++ boxes/hv_fv/provision.sh
@@ -8,7 +8,7 @@ apt-get install -y git ant build-essential
 apt-get install -y openjdk-7-jre-headless openjdk-7-jdk python3-psutil
 
 # Required for hvmonitor:
-apt-get install python3-setuptools
+apt-get -y install python3-setuptools
 
 echo "Installation (root) complete!"
 
